bfe6bc93e484943ce0b64653dd647936ba84827b
xsabduuh <abdosiifoou@gmail.com>
2026-04-03 18:22:32 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // إذا رفعت على GitHub Pages في مستودع اسمه "sakura-sushi"، غير "/" إلى "/sakura-sushi/"
-  base: "/Sushi_store/",
-});
