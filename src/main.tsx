3f8d64ea213fa70398baf3c27d2a8ab54bfa4026
xsabduuh <abdosiifoou@gmail.com>
2026-04-03 17:35:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./app";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root")!;
+
+window.onerror = (msg, src, line) => {
+  root.innerHTML = `<div style="color:red;padding:20px;font-size:18px">
+    ERROR: ${msg}<br/>Line: ${line}
+  </div>`;
+};
+
+createRoot(root).render(<App />);
