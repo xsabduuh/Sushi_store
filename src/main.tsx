d81c6fc9008f6b871d86b38136063404ccc7d661
xsabduuh <abdosiifoou@gmail.com>
2026-04-03 17:25:42 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./app";
 import "./index.css";
 
-const rootElement = document.getElementById("root")!;
-
-try {
-  const { createRoot: cr } = await import("react-dom/client");
-  createRoot(rootElement).render(<App />);
-} catch (e) {
-  rootElement.innerHTML = `<h1 style="color:red;padding:20px">${e}</h1>`;
-}
+createRoot(document.getElementById("root")!).render(<App />);
