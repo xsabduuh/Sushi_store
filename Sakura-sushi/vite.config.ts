bd5744a66cfe445432033eb8dd64e9a1c1c6a044
xsabduuh <abdosiifoou@gmail.com>
2026-04-03 05:52:36 +0100

    Create vite.config.ts

---
+++ Sakura-sushi/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // إذا رفعت على GitHub Pages في مستودع اسمه "sakura-sushi"، غير "/" إلى "/sakura-sushi/"
+  base: "/",
+});
